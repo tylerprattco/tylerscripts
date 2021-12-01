c93559340845ddcd512779de4186425177bab427
tylerprattco <87670197+tylerprattco@users.noreply.github.com>
2021-12-01 12:36:26 -0700

    Update colorpicker.sh

--- colorpicker.sh
+++ colorpicker.sh
@@ -1,3 +1,8 @@
 #!/bin/zsh
-
+#
+# Requires colorpicker and xclip (use pbcopy for mac)
+# selects color and outputs
 colorpicker --short --one-shot | xargs echo -n | xclip -selection c | xclip -o
+
+# mac script
+# colorpicker --short --one-shot | xargs echo -n | pbcopy
